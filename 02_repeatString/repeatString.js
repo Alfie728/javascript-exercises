f1c8e39ec24e41298132cc905213d538689614b6
Alfie728 <zegro728@gmail.com>
2024-02-10 15:53:16 +0800

    Completed exercises 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+  if (number < 0) {
+    return 'ERROR';
+  } else if (number === 0) {
+    return '';
+  } 
+  //else if (string === '') {
+  //   return '';
+  // }
+  return string.repeat(number);
 };
 
 // Do not edit below this line
